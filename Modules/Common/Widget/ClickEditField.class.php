b8b9f5ea9fe7756cb227b26f7d6a96659863592f
Nicolai Krüger <nicolaikr@hotmail.com>
2013-05-06 18:00:48 +0200

    Fix

--- Modules/Common/Widget/ClickEditField.class.php
+++ Modules/Common/Widget/ClickEditField.class.php
@@ -9,7 +9,7 @@ class Common_Widget_ClickEditField extends Widget {
   public function ToHtml() {
     return 	'<label id="' . $this->id . '">' . $this->value . '</label>'
 			. '<input id="' . $this->id . '_entry" style="display:none;"></input>'
-			. '<script src="./../../Includes/jquery-2.js"'
+			. '<script src="./../../Includes/jquery-2.js"></script>'
 			. '<script>'
 			. '$("#'.$this->id.'").click(function() {'
 		    	. '$("#'.$this->id.'").css("display", "none");'
