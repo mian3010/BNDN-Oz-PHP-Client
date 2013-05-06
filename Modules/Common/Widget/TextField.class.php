f3ab3d494a6dcce605ac095d0020fcb04778d741
Claus L. Henriksen <clih@itu.dk>
2013-05-06 16:18:15 +0200

    TextField

---
+++ Modules/Common/Widget/TextField.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class Common_Widget_TextField extends Widget {
+  public function __construct($label = ''){
+    $this->label = $label;
+    $this->default = "";
+  }
+  public function ToHtml() {
+    $returnVal = '';
+    if($this->label != '') { $returnVal = '<label for="' . $this->id . '">' . $this->label . ': </label>'; }
+    return $returnVal . ' <input type="text" name="tf_' . $this->id . '" id="' . $this->id . '" value="' . $this->default . '"/>';
+  }
+}
