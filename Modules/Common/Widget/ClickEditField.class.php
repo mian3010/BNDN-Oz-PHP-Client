38964e1c826e80caf5c9437244621d17c1177fe0
Nicolai Krüger <nicolaikr@hotmail.com>
2013-05-06 17:05:21 +0200

    EditOnClick field almost done

---
+++ Modules/Common/Widget/ClickEditField.class.php
@@ -0,0 +1,26 @@
+<?php
+
+class Common_Widget_ClickEditField extends Widget {
+  public function __construct($value = ''){
+    $this->value = $value;
+	$this->handler = '';
+  }
+  
+  public function ToHtml() {
+    return 	'<label id="' . $this->id . '">' . $this->value . ': </label>'
+			. '<input id="' . $this->id . '_entry" style="display:none;"></input>'
+			. '$("#'.$this->id.'").click(function() {'
+		    	. '$("#'.$this->id.'").css("display", "none");'
+		    	. '$("#'.$this->id.'_entry")'
+		    	. '.val($("#'.$this->id.'").text())'
+		    	. '.css("display", "")'
+		    	. '.focus();'
+	    	. '});'
+			. '$("#'.$this->id.'_entry").blur(function() {'
+		    	. '$("#'.$this->id.'_entry").css("display", "none");'
+		    	. '$("#'.$this->id.'")'
+		    	. '.text($("#'.$this->id.'_entry").val())'
+		    	. '.css("display", "");'
+	  		. '});';
+  }
+}
