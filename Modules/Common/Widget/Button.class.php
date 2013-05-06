acc5434c91a7308ac36eadfd10aee3374cd64da5
Claus L. Henriksen <clih@itu.dk>
2013-05-06 21:46:38 +0200

    fixed bug

--- Modules/Common/Widget/Button.class.php
+++ Modules/Common/Widget/Button.class.php
@@ -1,22 +1,16 @@
 <?php
 
 class Common_Widget_Button extends Widget {
-  public function __construct($value = '') {
-    $this->value = $value;
-    $this->type = '';
-    $this->disabled = '';
-    $this->formmethod = '';
-    $this->formaction = '';
-    $this->form = '';
-    $this->name = '';
+  public $atrbs = array();
+
+  public function __construct($text = '') {
+    $this->text = $text;
   }
 
   public function ToHtml() {
-    if ($this->type != '') { $this->type = ' type="' . $this->type . '" '; }
-    if ($this->formmethod != '') { $this->formmethod = ' formmethod="' . $this->formmethod . '" '; }
-    if ($this->formaction != '') { $this->formaction = ' formaction="' . $this->formaction . '" '; }
-    if ($this->form != '') { $this->form = ' form="' . $this->form . '" '; }
-    if ($this->name != '') { $this->name = ' name="' . $this->name . '" '; }
-    return '<button id="' . $this->id .'"' . $this->name . $this->type . $this->disabled . $this->form . $this->formmethod . $this->formaction . '>' . $this->value . '</button>';
+    $atrb = '';
+    foreach ($this->atrbs as $key => $value) $atrb .= $key . '="' . $value . '" ';
+
+    return '<button ' . $atrb . '>' . $this->text . '</button>';
   }
 }
