56b3120ef31d3024ff2c04d17e178bd8c0af7eaf
Michael Søby Andersen <michael@mileratec.com>
2013-05-06 14:01:23 +0200

    Container class implemented

--- Modules/Common/Widget/WidgetContainer.class.php
+++ Modules/Common/Widget/WidgetContainer.class.php
@@ -2,4 +2,10 @@
 
 abstract class WidgetContainer extends Widget {
   public $widgets; // Array of Widgets
+
+  protected function ChildrenToHtml() {
+    $markup = '';
+    foreach ($widgets as $widget) $markup .= $widget->ToHtml();
+    return $markup;
+  }
 }
