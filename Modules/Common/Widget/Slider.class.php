24893ee2d9d25688a87c9e5865b8804ee42eb873
Claus L. Henriksen <clih@itu.dk>
2013-05-07 22:01:57 +0200

    working on slider

--- Modules/Common/Widget/Slider.class.php
+++ Modules/Common/Widget/Slider.class.php
@@ -2,30 +2,47 @@
 
 class Common_Widget_Slider extends WidgetContainer {
   private $widget;
+  private $rotation;
+  private $rotationTo;
+  private $class;
+  private $show;
+  private $hide;
 
   public function __construct($widget) {
     $this->widget = $widget;
   }
 
-  public function ToHtml(){
-    // Create a wrapper and add the widget
-    $wrapper = new Common_Widget_Wrapper();
-    $wrapper->id = 'slider_' . $this->id;
-    $wrapper->widgets[$this->id] = $this->widget;
+  public function SlideRight(){
+    $this->rotation = 90;
+    $this->rotationTo = 180;
+    $this->class = 'right';
+    $this->hide = 'slideDown("slow")';
+    $this->show = 'slideUp()';
+  }
 
-    // Add slider functionality to the wrapper
+  public function SlideUp(){
+    $this->rotation = 90;
+    $this->rotationTo = 180;
+    $this->class = 'top';
+    $this->hide = 'slideDown("slow")';
+    $this->show = 'slideUp()';
+  }
+
+  public function ToHtml(){
+    $this->SlideRight();
+
     //return $this->widget->ToHtml();
-    return '<div id="sliderwrapper_' . $this->widget->id . '"> <div id="slider_' . $this->widget->id . '">' . $this->widget->ToHtml() . ' </div><div id="sliderbutton_' . $this->widget->id . '"></div></div>
+    return '<div id="sliderwrapper_' . $this->widget->id . '" class="' . $this->class . '"> <div id="slider_' . $this->widget->id . '">' . $this->widget->ToHtml() . ' </div><div id="sliderbutton_' . $this->widget->id . '"></div></div>
       <script src="http://code.jquery.com/jquery-latest.js"></script>
       <script>
 
           $(document).ready(function(){
             $("#sliderbutton_' . $this->widget->id . '").click(function () {
               if ($("#slider_' . $this->widget->id . '").is(":hidden")) {
-                $("#slider_' . $this->widget->id . '").slideDown("slow");
+                $("#slider_' . $this->widget->id . '").' . $this->hide . ';
                 $("#sliderbutton_' . $this->widget->id . '").removeClass("rotate");
               } else {
-                $("#slider_' . $this->widget->id . '").slideUp();
+                $("#slider_' . $this->widget->id . '").' . $this->show . ';
                 $("#sliderbutton_' . $this->widget->id . '").addClass("rotate");
               }
             });
@@ -37,7 +54,6 @@ class Common_Widget_Slider extends WidgetContainer {
             margin:3px;
             padding:3px;
             border-radius:5px;
-            text-align:center;
           }
           #sliderbutton_' . $this->widget->id . '{
             width: 0;
@@ -50,10 +66,21 @@ class Common_Widget_Slider extends WidgetContainer {
             clear:both;
           }
           .rotate{
-            -webkit-transform: rotate(180deg);
+            -webkit-transform: rotate(' . $this->rotationTo . 'deg);
           }
           #sliderwrapper_' . $this->widget->id . '{
             padding:5px;
+            text-align:center;
+            border:solid 5px black;
+          }
+          .right{
+            position:fixed;
+            margin-top:none;
+            margin-left:auto;
+            margin-right:none;
+            -webkit-transform: rotate(90deg);
+           }
+          .top{
             margin-left:auto;
             margin-right:auto;
           }
