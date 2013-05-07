f2de8ff4d932b83ad4734f784c12ddf26370fa09
Nicolai Krüger <nicolaikr@hotmail.com>
2013-05-07 14:27:33 +0200

    Made FixedWidget - not tested yet

--- Modules/Common/Widget/FixedWidget.class.php
+++ Modules/Common/Widget/FixedWidget.class.php
@@ -1,6 +1,21 @@
 <?php
 
 class Common_Widget_FixedWidget extends Widget {
-  public function __construct(){
+  public function __construct($widget){
+	$this->widget = $widget;
+	$this->top = 0;
+	$this->left = 0;
+  }
+  
+  public function SetTop($top) {
+  	$this->top = $top;
+  }
+  
+  public function SetLeft($left) {
+  	$this->left = $left;
+  }
+  
+  public function ToHtml() {
+  	return '<div style="position:fixed;top:"' . $this->top . '";left"'. $this->left . '">' . $this->widget.ToHtml() . '</div>';
   }
 }
