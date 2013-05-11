bbb1a3936074bd5879d6b90293d5bc75a7058e6e
Claus L. Henriksen <clih@itu.dk>
2013-05-11 21:27:04 +0200

    Working on Account View

--- Modules/Account/Widget/ViewAccount.class.php
+++ Modules/Account/Widget/ViewAccount.class.php
@@ -3,7 +3,27 @@
  * Widget for viewing a specific account by username
  */
 class Account_Widget_ViewAccount extends Widget_Container {
-  public function __construct() {
+  public function __construct($user, $editable = FALSE, $newAccount = FALSE) {
+    // Options
+    // Form
+    if($editable){
+      $f = new Widget_Form();
+      $f->id = "form-login";
+      $f->action = UriController::GetAbsolutePath('/Accounts/' . $user['username']);
+      $f->method = $newAccount ? 'POST' : 'PUT';
 
+      // Left slider if own
+      $leftContainer = new Widget_Container();
+      $cName = new Widget_Container();
+      $lName = new Widget_Label('Name');
+      $fName = new Widget_ClickEditField(isset($user['name']) ? $user['name'] : '');
+      $cName->widgets += array($lName, $fName);
+
+      $this->widgets[] = new Widget_Slider($leftContainer);
+    }
+    // About me
+    // Picture
+    // Right atrbs
+    // Add to form
   }
 }
