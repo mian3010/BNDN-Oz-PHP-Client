e0c9ec2bb38cf8e8ee7b3c71c79df00eddca20ae
Michael Søby Andersen <michael@mileratec.com>
2013-05-11 13:21:12 +0200

    Added helper for testing protected

--- Testing/Testing_Base.class.php
+++ Testing/Testing_Base.class.php
@@ -5,7 +5,19 @@ class Testing_Base extends PHPUnit_Framework_TestCase {
     set_include_path(get_include_path() . PATH_SEPARATOR . realpath('..'));
     require_once('Includes/Common.php');
   }
+
+  /*
+   * Helper method for returning an inaccesible method, and being able to call 
+   * it
+   */  
+  protected function GetMethod($class, $method) {
+    $class = new ReflectionClass($class);
+    $method = $class->getMethod($method);
+    $method->setAccessible(true);
+    return $method;
+  }
+  
   public function testDummy() {
 
-  }
+  } 
 }
