b1c2e023a035b20e1b7ef6634228fbb2a6570036
Michael Søby Andersen <michael@mileratec.com>
2013-05-11 15:30:53 +0200

    Purchases model should be done

--- Modules/Purchase/Model/Default.class.php
+++ Modules/Purchase/Model/Default.class.php
@@ -1,11 +1,24 @@
 <?php
 
 class Purchase_Model_Default extends CommonModel {
-  public function GetPurchases($token) {
-    throw new NotImplementedException();
+  public function GetPurchases($username, $buyrent, $token, $info = 'more') {
+    $ws = new WebService('accounts/'.$username.'/purchases', 'GET');
+    $ws->SetData(array(
+      'info' => $info,
+      'purchases' => $buyrent,
+    ));
+    $ws->SetToken($token);
+    $purchases = $ws->Execute();
+    $this->ThrowExceptionIfError($ws->GetHttpStatusCode());
+    return $purchases;
   }
 
-  public function CreatePurchases($token) {
-    throw new NotImplementedException();
+  public function CreatePurchases($username, $purchases, $token) {
+    $ws = new WebService('accounts/'.$username.'/purchases', 'POST');
+    $ws->SetData($purchases);
+    $ws->SetToken($token);
+    $purchases = $ws->Execute();
+    $this->ThrowExceptionIfError($ws->GetHttpStatusCode());
+    return $purchases;
   }
 }
