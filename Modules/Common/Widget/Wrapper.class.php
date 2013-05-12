22d2294ef60677ec87317f579cad3b74d56af5b5
Claus L. Henriksen <clih@itu.dk>
2013-05-12 23:56:19 +0200

    title changed to wrapperTitle

--- Modules/Common/Widget/Wrapper.class.php
+++ Modules/Common/Widget/Wrapper.class.php
@@ -1,12 +1,14 @@
 <?php
 
 class Widget_Wrapper extends WidgetContainer {
+  public $wrapperTitle;
+
   public function ToHtml() {
-    if(trim($this->GetTitle()) != '')
-      $t = '<h2 class="title">' . $this->GetTitle() . '</h2>';
+    if(trim($this->wrapperTitle) != '')
+      $t = '<h2 class="wrapperTitle">' . $this->wrapperTitle . '</h2>';
     else
       $t = '';
 
-    return '<div ' . $this->GetAttributes() . '">'.$this->ChildrenToHtml().'</div>';
+    return '<div ' . $this->GetAttributes() . '">' . $t . $this->ChildrenToHtml().'</div>';
   }
 }
