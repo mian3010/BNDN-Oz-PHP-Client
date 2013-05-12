46a22e82f38ae62aaafdd0f647585b1bdddac258
Michael Søby Andersen <michael@mileratec.com>
2013-05-12 12:14:35 +0200

    JQuery file in addjsfile. Rest should use addjs too

--- Modules/Common/Widget/ClickEditField.class.php
+++ Modules/Common/Widget/ClickEditField.class.php
@@ -3,7 +3,8 @@
 class Widget_ClickEditField extends Widget {
   public function __construct($value = ''){
     $this->value = $value;
-	$this->handler = '';
+	  $this->handler = '';
+    $this->AddJsFile('/static/js/jquery-2.js');
   }
   
   public function addHandler($handler) {
@@ -13,7 +14,6 @@ class Widget_ClickEditField extends Widget {
   public function ToHtml() {
     return 	'<label id="' . $this->id . '">' . $this->value . '</label>'
 			. '<input id="' . $this->id . '_entry" style="display:none;"></input>'
-			. '<script src="./../../Includes/jquery-2.js"></script>'
 			. '<script>'
 			. '$("#'.$this->id.'").click(function() {'
 		    	. '$("#'.$this->id.'").css("display", "none");'
