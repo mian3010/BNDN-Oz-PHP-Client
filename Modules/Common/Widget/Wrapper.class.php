7037a1ccfc04e14f30eecee4e2586af78bd138b4
Claus L. Henriksen <clih@itu.dk>
2013-05-12 16:51:35 +0200

    h2 title

--- Modules/Common/Widget/Wrapper.class.php
+++ Modules/Common/Widget/Wrapper.class.php
@@ -2,6 +2,11 @@
 
 class Widget_Wrapper extends WidgetContainer {
   public function ToHtml() {
-    return '<div ' . $this->GetAttributes() . $this->GetClasses() . '">'.$this->ChildrenToHtml().'</div>';
+    if(trim($this->GetTitle()) != '')
+      $t = '<h2 class="title">' . $this->GetTitle() . '</h2>';
+    else
+      $t = '';
+
+    return '<div ' . $this->GetAttributes() . $this->GetClasses() . '">' . $t . $this->ChildrenToHtml().'</div>';
   }
 }
