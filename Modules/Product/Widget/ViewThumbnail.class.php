1a017fdf94f3ce6e634712516a51b1c52d108925
Michael Søby Andersen <michael@mileratec.com>
2013-05-14 17:25:26 +0200

    Changed to use alternative image instead

--- Modules/Product/Widget/ViewThumbnail.class.php
+++ Modules/Product/Widget/ViewThumbnail.class.php
@@ -2,11 +2,7 @@
 
 class Product_Widget_ViewThumbnail extends Widget_Image {
   public function __construct($id) {
-    try {
-      parent::__construct(WebService::GetAbsolute("/products/".$id."/thumbnail"));
-    } catch (ImageException $e) {
-      parent::__construct('static/img/accountThumb.jpg');
-    }
+    parent::__construct(WebService::GetAbsolute("/products/".$id."/thumbnail"), 'static/img/accountThumb.jpg');
     $this->width = 100;
     $this->height = 100;
     $this->classes[] = 'left';
