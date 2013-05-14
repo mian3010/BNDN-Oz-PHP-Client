b7305ef98f1ff38cb9d21875fb85c2d4a43cd3dc
Nicolai Krüger <nicolaikr@hotmail.com>
2013-05-14 16:05:07 +0200

    BuyCredits widget is still in progress

--- Modules/Credits/Widget/BuyCredits.class.php
+++ Modules/Credits/Widget/BuyCredits.class.php
@@ -4,7 +4,13 @@
  * Widget for buying credits
  */
 class Credits_Widget_BuyCredits extends Widget_Container {
- public function ToHtml() {
-   throw new NotImplementedException();
- }
+ public function __construct() {
+ 	$this->widgets[] = new Widget_InputField("number");
+	$this->widgets[] = new Widget_Label(" C");
+	
+	$t = new Widget_ThreePartButton("", "1", "2", "3");
+	$this->widgets[] = $t;
+	
+	$this->widgets[] = new Widget_Button("Buy");
+  }
 }
