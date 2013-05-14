63e6a6726e33b780aa08efe565143a3641282201
Nicolai Krüger <nicolaikr@hotmail.com>
2013-05-14 16:14:35 +0200

    Hotfix

--- Modules/Common/Widget/Link.class.php
+++ Modules/Common/Widget/Link.class.php
@@ -3,9 +3,10 @@
 class Widget_Link extends WidgetContainer {
   public function __construct($href) {
     $this->href = $href;
-	$this->disable = false;
-	
-	$js = <<<JS
+  }
+
+  public function ToHtml() {
+  	$js = <<<JS
       $(function () {
     	$('#$this->id').on("click", function (e) {
         	if($this->disable) {
@@ -16,9 +17,7 @@ class Widget_Link extends WidgetContainer {
 	
 JS;
 	$this->AddJs($js);
-  }
-
-  public function ToHtml() {
+	
     return '<a ' . $this->GetAttributes() . '>' . $this->ChildrenToHtml() . '</a>';
   }
 }
