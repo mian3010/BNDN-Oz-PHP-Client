49fd20f50801e0f5a6e2ae959fca751be779f316
Nicolai Krüger <nicolaikr@hotmail.com>
2013-05-14 16:02:51 +0200

    Added possibility to disable a link

--- Modules/Common/Widget/Link.class.php
+++ Modules/Common/Widget/Link.class.php
@@ -3,6 +3,19 @@
 class Widget_Link extends WidgetContainer {
   public function __construct($href) {
     $this->href = $href;
+	$this->disable = false;
+	
+	$js = <<<JS
+      $(function () {
+    	$('#$this->id').on("click", function (e) {
+        	if($this->disable) {
+        		e.preventDefault();
+        	}
+    	});
+	});
+	
+JS;
+	$this->AddJs($js);
   }
 
   public function ToHtml() {
