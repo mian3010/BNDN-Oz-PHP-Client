c28e2358b058b0213a78a4e52a45ca6b4d3e9f51
Michael Søby Andersen <michael@mileratec.com>
2013-05-14 14:14:27 +0200

    Small error fixed in it

--- Modules/Product/Widget/ViewThumbnail.class.php
+++ Modules/Product/Widget/ViewThumbnail.class.php
@@ -5,9 +5,10 @@ class Product_Widget_ViewThumbnail extends Widget_Image {
     try {
       parent::__construct(WebService::GetAbsolute("/products/".$id."/thumbnail"));
     } catch (ImageException $e) {
-      parent::_construct('static/img/accountThumb.jpg');
+      parent::__construct('static/img/accountThumb.jpg');
     }
     $this->width = 100;
     $this->height = 100;
+    $this->classes[] = 'left';
   }
 }
