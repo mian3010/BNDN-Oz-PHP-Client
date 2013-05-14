bb1bd8049c2638e188a06e60a5fa50151a81d23f
Nicolai Krüger <nicolaikr@hotmail.com>
2013-05-14 18:16:14 +0200

    Michael piver...

--- Modules/Credits/Widget/ViewCredits.class.php
+++ Modules/Credits/Widget/ViewCredits.class.php
@@ -5,6 +5,6 @@
 class Credits_Widget_ViewCredits extends Widget_Container {
   public function __construct($c) {
     if(isset($c)) { $this->c = $c; } else { $this->c = 0; }
-    $this->widgets[] = new Widget_ThreePartButton('/Credits/BuyCredits', $this->c, "C", "&nbsp;");
+    $this->widgets[] = new Widget_ThreePartButton('/Credits/BuyCredits', "&nbsp;", $this->c, "C");
   }
 }
