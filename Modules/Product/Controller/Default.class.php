563c396d09d19ceda138e8f2c66528feb3b25c60
Michael Søby Andersen <michael@mileratec.com>
2013-05-16 11:18:09 +0200

    Browse products replaced with ViewTypes

--- Modules/Product/Controller/Default.class.php
+++ Modules/Product/Controller/Default.class.php
@@ -17,7 +17,7 @@ class Product_Controller_Default extends CommonController {
 	public function View($id = 0) {
     if($id == 0){
       RentItError('Product not found');
-      RentItGoto('Product', 'Browse');
+      RentItGoto('Product', 'ViewTypes');
     }
 		try {
       if(!isset($_SESSION['token'])) {
@@ -27,7 +27,7 @@ class Product_Controller_Default extends CommonController {
 			$product = $this->productModel->GetProduct($id, $_SESSION['token']->token);
       if($product == null) {
         RentItError('Product not found');
-        RentItGoto('Product', 'Browse');
+        RentItGoto('Product', 'ViewTypes');
       }
       $edit = FALSE;
       if((isset($_SESSION['username'])) && strtolower($_SESSION['username']) == $product->owner) $edit = TRUE;
@@ -40,7 +40,7 @@ class Product_Controller_Default extends CommonController {
 		} catch (Exception $e) {
 			  RentItError('Server error');
 		}
-	RentItGoto('Product', 'Browse');
+	RentItGoto('Product', 'ViewTypes');
 	}
 	
 	/**
--- Modules/Product/Widget/ViewProducts.class.php
+++ Modules/Product/Widget/ViewProducts.class.php
@@ -6,7 +6,7 @@
 class Product_Widget_ViewProducts extends Widget_Container {
   public function __construct($products) {
     $this->SetTitle("Product list");
-    $this->AddOption("Search products", "Product/Browse/");
+    $this->AddOption("Search products", "Product/Search/");
     $pw = new Widget_Wrapper();
     foreach ($products as $product) {
       $pw->widgets[] = new Product_Widget_SmallViewProduct($product);
