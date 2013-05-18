24bd44a413b87c456fb224ef9a60441ecbaf97a9
Michael Søby Andersen <michael@mileratec.com>
2013-05-18 16:00:34 +0200

    Documentation on Purhase model

--- Modules/Purchase/Model/Default.class.php
+++ Modules/Purchase/Model/Default.class.php
@@ -1,6 +1,14 @@
 <?php
 
 class Purchase_Model_Default extends CommonModel {
+  /**
+   * Get all purchases
+   * @param $username The username to get purchases by
+   * @param $token The token to send along
+   * @param $buyrent Whether or not to include both buy and rent
+   * @param $info Level of detail
+   * @return Array of purchases
+   */
   public function GetPurchases($username, $token, $buyrent = "BR", $info = 'more') {
     $ws = new WebService('accounts/'.$username.'/purchases', 'GET');
     $ws->SetData(array(
@@ -12,7 +20,14 @@ class Purchase_Model_Default extends CommonModel {
     $this->ThrowExceptionIfError($ws->GetHttpStatusCode());
     return $purchases;
   }
-  
+
+  /**
+   * Get a single purchase
+   * @param $username The username to get purchase by
+   * @param $token Token to send along
+   * @param $tId The purchase id
+   * @return The single purchase
+   */
   public function GetPurchase($username, $token, $tId) {
     $ws = new WebService('accounts/'.$username.'/purchases/'.$tId, 'GET');
     if($token!=null) $ws->SetToken($token);
@@ -22,6 +37,13 @@ class Purchase_Model_Default extends CommonModel {
     return $object;
   }
 
+  /**
+   * Get a purchase by product id
+   * @param $username The username to get purchase by
+   * @param $token The token to send along 
+   * @param $pId The product id to get purchase by
+   * @return The single purchase
+   */
   public function GetPurchaseByPid($username, $token, $pId) {
     $purchases = $this->GetPurchases($username, $token);
     foreach ($purchases as $purchase) {
@@ -30,6 +52,13 @@ class Purchase_Model_Default extends CommonModel {
     return null;
   }
 
+  /**
+   * Create a list of purchases
+   * @param $username The username to create purchase for
+   * @param $purchases The purchases to create
+   * @param $token Token to send along
+   * @return The purchases
+   */
   public function CreatePurchases($username, $purchases, $token) {
     $ws = new WebService('accounts/'.$username.'/purchases', 'POST');
     $ws->SetData($purchases);
