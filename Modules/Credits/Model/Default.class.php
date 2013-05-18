d8902084f0e2d96e02109a8c601ce2e82bf8760e
Michael Søby Andersen <michael@mileratec.com>
2013-05-18 15:02:50 +0200

    Documentation on Credits model

--- Modules/Credits/Model/Default.class.php
+++ Modules/Credits/Model/Default.class.php
@@ -5,6 +5,7 @@ class Credits_Model_Default extends CommonModel {
    * Add credits to an account
    * @param $token Authentication token with account information
    * @param $amount Amount of credits to be added
+   * @return TRUE
    */
   public function UpdateCredits($token, $amount) {
     $ws = new WebService('credits', 'POST');
