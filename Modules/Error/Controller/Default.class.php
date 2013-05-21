665c9fcd5e3b3395616d7133a9e74cc110e2e252
Michael Søby Andersen <michael@mileratec.com>
2013-05-21 13:23:44 +0200

    Changes in error page

--- Modules/Error/Controller/Default.class.php
+++ Modules/Error/Controller/Default.class.php
@@ -3,9 +3,8 @@
 class Error_Controller_Default extends CommonController {
   public function Fatal() {
     $t = new Widget_Text("The server encountered an unexpected error.\n&nbsp;\n
-      You have been redirected to this page to avoid calling the server again, failing and redirecting.\n&nbsp;\n
-      The cause could be that the server is down. Please try again later");
-    $t->style = "color: #ff5b5b; font-weight: bold; font-size: 14px;";
+      The server could be down. Please try again later");
+    $t->style = "color: #A63400; font-weight: bold; font-size: 14px;";
     $t->SetTitle('Fatal error');
     $t->AddOption('Retry', '');
     return $t;
